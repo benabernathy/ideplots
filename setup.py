9e70b262524e7aa405961e787ff101e4c6c8e665
Ben Abernathy <ben.abernathy@gmail.com>
2023-11-21 14:55:54 -0700

    updated version 0.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(root, 'README.md'), 'r', encoding='utf-8') as f:
 
 setup(
     name='ideplots',
-    version='0.1.0',
+    version='0.1.1',
     author="Ben Abernathy",
     author_email="ben.abernathy@gmail.com",
     maintainer="Ben Abernathy",
